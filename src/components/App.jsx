6b709c5b044770d225b3d64c9a3389e924d2319b
SoyJuju <julianthant@gmail.com>
2023-10-05 23:39:06 +0630

    lazy loading

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,38 @@
-import Dashboard from './main/Dashboard';
-import Login from './authentication/Login';
-import Homepage from './home/Homepage';
-import ForgotPassword from './authentication/ForgotPassword';
-import Signup from './authentication/Signup';
+import { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+const Homepage = lazy(() => import('./home/Homepage'));
+const Dashboard = lazy(() => import('./main/Dashboard'));
+const Login = lazy(() => import('./authentication/Login'));
+const ForgotPassword = lazy(() => import('./authentication/ForgotPassword'));
+const Signup = lazy(() => import('./authentication/Signup'));
 import PrivateRoute from '../hooks/PrivateRoute';
 import { FirebaseProvider } from '../contexts/FirebaseContexts';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import '../index.css';
 import Navbar from './main/Navbar';
-import AccountCenter from './authentication/AccountCenter';
-import EmailVerification from './authentication/EmailVerification';
+const AccountCenter = lazy(() => import('./authentication/AccountCenter'));
+const EmailVerification = lazy(() =>
+  import('./authentication/EmailVerification')
+);
 
 function App() {
   return (
     <Router>
-      <FirebaseProvider>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" element={<PrivateRoute />}>
-            <Route index element={<Dashboard />} />
-          </Route>
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/verify-email" element={<EmailVerification />} />
-          <Route path="/action-center" element={<AccountCenter />} />
-        </Routes>
-      </FirebaseProvider>
+      <Suspense fallback={<div>Loading...</div>}>
+        <FirebaseProvider>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/dashboard" element={<PrivateRoute />}>
+              <Route index element={<Dashboard />} />
+            </Route>
+            <Route path="/forgot-password" element={<ForgotPassword />} />
+            <Route path="/verify-email" element={<EmailVerification />} />
+            <Route path="/action-center" element={<AccountCenter />} />
+          </Routes>
+        </FirebaseProvider>
+      </Suspense>
     </Router>
   );
 }
