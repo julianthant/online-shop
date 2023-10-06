3ebeafa51955cb429b755d2e784a2e3be8946d19
SoyJuju <julianthant@gmail.com>
2023-10-06 23:35:27 +0630

    make new arrivals to shoe grid reusable

---
+++ src/components/home/ShoeGrid.jsx
@@ -0,0 +1,46 @@
+import ShoeCard from './ShoeCard';
+import PropTypes from 'prop-types';
+
+export default function ShoeGrid({ Heading, Subheading, Data }) {
+  return (
+    <div className="container py-8">
+      <div className="flex justify-between items-center">
+        <h2 className="font-[Poppins] text-2xl text-slate-50">{Heading}</h2>
+        <a className="underline underline-offset-[6px] max-s:hidden decoration-emerald-600 text-slate-50 hover:decoration-slate-50 transition-300 text-md tracking-[0.2rem] cursor-pointer">
+          {Subheading}
+        </a>
+        <a className="underline underline-offset-[6px] s:hidden decoration-emerald-600 text-slate-50 hover:decoration-slate-50 transition-300 text-md tracking-[0.2rem] cursor-pointer">
+          VIEW ALL
+        </a>
+      </div>
+      <div className="pt-10 shoe-grid">
+        {Data.map((shoe, index) => (
+          <ShoeCard
+            key={index}
+            brand={shoe.brand}
+            name={shoe.name}
+            price={shoe.price}
+            description={shoe.description}
+            image={shoe.image}
+            tag={shoe.tag}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+ShoeGrid.propTypes = {
+  Heading: PropTypes.string.isRequired,
+  Subheading: PropTypes.string.isRequired,
+  Data: PropTypes.arrayOf(
+    PropTypes.shape({
+      brand: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      price: PropTypes.number.isRequired,
+      description: PropTypes.string.isRequired,
+      image: PropTypes.string.isRequired,
+      tag: PropTypes.bool,
+    })
+  ).isRequired,
+};
