0b56cf8fe8d6f639ca4b8a50b85bb2134024b1cd
SoyJuju <julianthant@gmail.com>
2023-10-06 14:15:51 +0630

    new shoe card design

--- src/components/home/ShoeCard.jsx
+++ src/components/home/ShoeCard.jsx
@@ -1,21 +1,35 @@
+import { Link } from 'react-router-dom';
 import shoeImage from '../../assets/NB550.jpg';
 
 export default function ShoeCard() {
   return (
-    <div>
-      <div className="w-[23.5rem] pb-4 bg-slate-50">
-        <div className="pt-16 py-4 bg-[#c2bebe69]">
-          <img className="px-6 max-w-full max-h-full" src={shoeImage} alt="" />
+    <Link
+      to="/shoe-details"
+      className="w-[23.5rem] bg-white overflow-hidden shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:scale-105"
+    >
+      <div className="relative">
+        <div className="bg-gray-200">
+          <img
+            className="w-full h-auto px-4 pt-16 pb-10"
+            src={shoeImage}
+            alt="New Balance 550"
+          />
         </div>
-        <div className="font-[Inter] pt-4 flex flex-col items-center gap-3">
-          <h3 className="text-slate-950 text-center font-semibold text-lg">
-            New Balance 550
-          </h3>
-          <p className="text-slate-950 text-center font-semibold text-lg">
-            $99.90
-          </p>
+        <div className="absolute top-0 right-0 px-4 py-2 bg-emerald-900 text-white font-bold rounded-bl-lg">
+          New
         </div>
       </div>
-    </div>
+      <div className="p-4 font-[Poppins]">
+        <h3 className="text-gray-900 text-xl font-semibold mb-2">
+          New Balance 550
+        </h3>
+        <p className="text-gray-700 text-lg">$99.90</p>
+        <p className="text-gray-600 mt-2">
+          The New Balance 550 is a classic sneaker with a timeless design.
+          It&apos;s perfect for both casual and athletic wear, offering comfort
+          and style in one package.
+        </p>
+      </div>
+    </Link>
   );
 }
