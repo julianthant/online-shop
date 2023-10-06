0b634a1235a19c8a663023adaba2a3de9f594227
SoyJuju <julianthant@gmail.com>
2023-10-06 23:36:35 +0630

    changed from json to jsx

---
+++ src/data/NewShoes.jsx
@@ -0,0 +1,65 @@
+import NB550 from '../assets/NewShoes/NB550.webp';
+import AJ4 from '../assets/NewShoes/AJ4.webp';
+import Ultraboost from '../assets/NewShoes/Ultraboost.webp';
+import AirMax90 from '../assets/NewShoes/AirMax90.webp';
+import LowlandCC from '../assets/NewShoes/LowlandCC.webp';
+import ChuckTaylorAllStar from '../assets/NewShoes/ChuckTaylorAllStar.webp';
+
+let NewShoes = [
+  {
+    brand: 'Nike',
+    name: 'AJ 4 Dior',
+    price: 129.99,
+    description:
+      'The Nike AJ 4 is a classic sneaker known for its iconic design and high-flying style.',
+    image: AJ4,
+    tag: true,
+  },
+  {
+    brand: 'Adidas',
+    name: 'Ultra Boost',
+    price: 159.99,
+    description:
+      'The Adidas Ultra Boost offers unmatched energy return and a sleek, modern look.',
+    image: Ultraboost,
+    tag: true,
+  },
+  {
+    brand: 'Vans',
+    name: 'Lowland CC',
+    price: 164.95,
+    description:
+      'The Vans Lowland CC is a classic skate shoe with a timeless design.',
+    image: LowlandCC,
+    tag: true,
+  },
+  {
+    brand: 'New Balance',
+    name: '550',
+    price: 174.99,
+    description:
+      'The New Balance 550 is a premium shoe known for its comfort and stability.',
+    image: NB550,
+    tag: true,
+  },
+  {
+    brand: 'Converse',
+    name: 'Chuck Taylor All Star',
+    price: 149.99,
+    description:
+      'The Converse Chuck Taylor All Star is an iconic and versatile canvas sneaker.',
+    image: ChuckTaylorAllStar,
+    tag: true,
+  },
+  {
+    brand: 'Nike',
+    name: 'Air Max 90',
+    price: 199.99,
+    description:
+      'The Nike Air Max 90 is a classic sneaker known for its comfort and iconic design.',
+    image: AirMax90,
+    tag: true,
+  },
+];
+
+export default NewShoes;
