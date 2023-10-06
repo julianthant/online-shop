04bafe9f5e006229f79a20cd0dc83e816d214281
SoyJuju <julianthant@gmail.com>
2023-10-06 23:36:12 +0630

    completed shoe card

--- src/components/home/ShoeCard.jsx
+++ src/components/home/ShoeCard.jsx
@@ -1,35 +1,49 @@
-import { Link } from 'react-router-dom';
-import shoeImage from '../../assets/NB550.jpg';
+import PropTypes from 'prop-types';
 
-export default function ShoeCard() {
+export default function ShoeCard({
+  brand,
+  name,
+  price,
+  description,
+  image,
+  tag,
+}) {
   return (
-    <Link
+    <div
       to="/shoe-details"
-      className="w-[23.5rem] bg-white overflow-hidden shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:scale-105"
+      className="bg-white overflow-hidden shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:scale-105"
     >
       <div className="relative">
-        <div className="bg-gray-200">
+        <div className="bg-gray-200 h-[20rem] flex items-center">
           <img
-            className="w-full h-auto px-4 pt-16 pb-10"
-            src={shoeImage}
+            loading="lazy"
+            className="w-full h-auto px-4"
+            src={image}
             alt="New Balance 550"
           />
         </div>
-        <div className="absolute top-0 right-0 px-4 py-2 bg-emerald-900 text-white font-bold rounded-bl-lg">
-          New
-        </div>
+        {tag && (
+          <div className="absolute top-0 right-0 pl-4 pr-3 py-2 bg-emerald-700 text-white font-bold rounded-bl-lg font-[Montserrat] tracking-wider">
+            New
+          </div>
+        )}
       </div>
       <div className="p-4 font-[Poppins]">
         <h3 className="text-gray-900 text-xl font-semibold mb-2">
-          New Balance 550
+          {brand} {name}
         </h3>
-        <p className="text-gray-700 text-lg">$99.90</p>
-        <p className="text-gray-600 mt-2">
-          The New Balance 550 is a classic sneaker with a timeless design.
-          It&apos;s perfect for both casual and athletic wear, offering comfort
-          and style in one package.
-        </p>
+        <p className="text-gray-700 text-lg">${price}</p>
+        <p className="text-gray-600 mt-2">{description}</p>
       </div>
-    </Link>
+    </div>
   );
 }
+
+ShoeCard.propTypes = {
+  brand: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  description: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  tag: PropTypes.bool,
+};
