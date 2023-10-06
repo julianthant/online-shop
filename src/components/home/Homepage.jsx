dd5041bd665374431297d7dcae2208f1972d913b
SoyJuju <julianthant@gmail.com>
2023-10-06 23:35:56 +0630

    added carousel and made order changes

--- src/components/home/Homepage.jsx
+++ src/components/home/Homepage.jsx
@@ -1,15 +1,28 @@
 import FeaturedBrands from './FeaturedBrands';
 import Heading from './Heading';
-import NewArrivals from './NewArrivals';
+import ShoeGrid from './ShoeGrid';
 import PickShoesByGender from './PickShoesByGender';
+import ShoeCarousel from './ShoeCarousel';
+
+import NewShoes from '../../data/NewShoes';
 
 export default function Homepage() {
   return (
     <section className="bg-matte-black">
       <Heading />
       <FeaturedBrands />
+      <ShoeCarousel />
+      <ShoeGrid
+        Heading="Latest Arrivals"
+        Subheading="VIEW ALL NEW ARRIVALS"
+        Data={NewShoes}
+      />
       <PickShoesByGender />
-      <NewArrivals />
+      <ShoeGrid
+        Heading="Best Sellers"
+        Subheading="VIEW ALL BEST SELLERS"
+        Data={NewShoes}
+      />
     </section>
   );
 }
