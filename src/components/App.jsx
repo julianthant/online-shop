dfa7117445dc04aaa8e191ac4a7a01414e338ab6
SoyJuju <julianthant@gmail.com>
2023-10-07 16:43:25 +0630

    added fallback for suspense

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <Router>
       <FirebaseProvider>
         <Navbar />
-        <Suspense>
+        <Suspense fallback={<Navbar />}>
           <Routes>
             <Route path="/" element={<Homepage />} />
             <Route path="/signup" element={<Signup />} />
