688e2034a2284bad1b8c2123c3f943853c2c016c
SoyJuju <julianthant@gmail.com>
2023-10-07 15:47:32 +0630

    new best shoes data and images

---
+++ src/data/BestShoes.jsx
@@ -0,0 +1,58 @@
+import NB990v3 from '../assets/BestShoes/NB990v3.webp';
+import AJ1 from '../assets/BestShoes/AJ1.webp';
+import Chuck70VintageCanvas from '../assets/BestShoes/Chuck70VintageCanvas.webp';
+import VansAuthentic from '../assets/BestShoes/VansAuthentic.webp';
+import AdidasEQTCSG91 from '../assets/BestShoes/EQT-CSG-91.webp';
+import NB5740 from '../assets/BestShoes/NB5740.webp';
+
+let BestShoes = [
+  {
+    brand: 'New Balance',
+    name: '990v3',
+    price: 199.99,
+    description:
+      'The New Balance 990v3 is a classic and comfortable running shoe.',
+    image: NB990v3,
+  },
+  {
+    brand: 'Nike',
+    name: 'Air Jordan 1',
+    price: 149.99,
+    description:
+      'The Nike Air Jordan 1 is an iconic and timeless basketball shoe.',
+    image: AJ1,
+  },
+  {
+    brand: 'Converse',
+    name: 'Chuck 70 Vintage Canvas',
+    price: 129.95,
+    description:
+      'The Converse Chuck 70 Vintage Canvas offers a retro look and feel.',
+    image: Chuck70VintageCanvas,
+  },
+  {
+    brand: 'Vans',
+    name: 'Authentic',
+    price: 59.99,
+    description:
+      'The Vans Authentic is a classic and versatile canvas sneaker.',
+    image: VansAuthentic,
+  },
+  {
+    brand: 'Adidas',
+    name: 'EQT-CSG-91',
+    price: 169.99,
+    description: 'The Adidas EQT-CSG-91 combines style and performance.',
+    image: AdidasEQTCSG91,
+  },
+  {
+    brand: 'New Balance',
+    name: '5740',
+    price: 139.99,
+    description:
+      'The New Balance 5740 offers comfort and style for everyday wear.',
+    image: NB5740,
+  },
+];
+
+export default BestShoes;
