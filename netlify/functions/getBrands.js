a3ddb81474f19db15b77f76aaf08c980d2b8762b
SoyJuju <julianthant@gmail.com>
2023-10-09 01:39:04 +0630

    fixed getBrands

--- netlify/functions/getBrands.js
+++ netlify/functions/getBrands.js
@@ -1,11 +1,8 @@
-// netlify/functions/sneakers.js
-
 import axios from 'axios';
 import https from 'https';
 
 export async function handler(event, context) {
-  const { path } = event;
-  const apiUrl = 'https://app.retailed.io' + path;
+  const apiUrl = 'https://app.retailed.io/api/v1/sneakers/brands';
 
   const axiosConfig = {
     headers: {
@@ -18,10 +15,6 @@ export async function handler(event, context) {
   try {
     const response = await axios.get(apiUrl, axiosConfig);
 
-    // Log the request and response for debugging
-    console.log('API URL:', apiUrl);
-    console.log('Response:', response.status, response.data);
-
     // Return a response
     return {
       statusCode: response.status,
@@ -29,9 +22,6 @@ export async function handler(event, context) {
     };
   } catch (error) {
     // Handle errors and send an error response
-    console.error('Error:', error);
-
-    // Set the HTTP status code based on the error response or default to 500
     const statusCode = error.response?.status || 500;
     return {
       statusCode,
