3d6969855d5aa917f85f71bf39bf43452884b333
SoyJuju <julianthant@gmail.com>
2023-10-09 00:21:42 +0630

    added netlify link

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const port = 3001;
 
 app.use(
   cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'https://solesteals.netlify.app'],
     methods: 'GET',
     credentials: true,
   })
