8b4a4e133ea02b5b8a1325b8c28368eb62cb98d7
SoyJuju <julianthant@gmail.com>
2023-10-08 23:50:28 +0630

    functions to make requests to sneaker api

---
+++ src/functions/GetBrands.jsx
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+export default async function GetSneakers(setBrand) {
+  const headers = {
+    'x-api-key': 'cefe79da-294d-4afa-b530-69f24ad08e64',
+  };
+
+  try {
+    const response = await axios.get(
+      `${import.meta.env.VITE_API_ORIGIN}/api/v1/sneakers/brands`,
+      { headers: headers, maxBodyLength: Infinity }
+    );
+    console.log(response.data.data);
+    setBrand(response.data.data);
+  } catch (error) {
+    console.error('Error fetching brands:', error);
+  }
+}
---
+++ src/functions/GetSneakers.jsx
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export default async function GetSneakers(setSneakers, brand_id) {
+  const headers = {
+    'x-api-key': 'cefe79da-294d-4afa-b530-69f24ad08e64',
+  };
+
+  try {
+    const response = await axios.get(
+      `${
+        import.meta.env.VITE_API_ORIGIN
+      }/api/v1/sneakers?brand_id=${brand_id}&extended=true`,
+      { headers: headers, maxBodyLength: Infinity }
+    );
+    console.log(response.data.data);
+    setSneakers(response.data.data);
+  } catch (error) {
+    console.error('Error fetching sneakers:', error);
+  }
+}
