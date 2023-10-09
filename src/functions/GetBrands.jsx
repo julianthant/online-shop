eeca0fd86cdf77f467471507ec6302caced92fc5
SoyJuju <julianthant@gmail.com>
2023-10-09 11:28:28 +0630

    upgraded toml file and links

--- src/functions/GetBrands.jsx
+++ src/functions/GetBrands.jsx
@@ -6,12 +6,9 @@ export default async function GetSneakers(setBrand) {
   };
 
   try {
-    const response = await axios.get(
-      'https://solesteals.netlify.app/.netlify/functions/getBrands',
-      {
-        headers: headers,
-      }
-    );
+    const response = await axios.get('/.netlify/functions/getBrands', {
+      headers: headers,
+    });
     console.log(response.data.data);
     setBrand(response.data.data);
   } catch (error) {
--- src/functions/GetSneakers.jsx
+++ src/functions/GetSneakers.jsx
@@ -7,7 +7,7 @@ export default async function GetSneakers(setSneakers, brand_id) {
 
   try {
     const response = await axios.get(
-      `https://solesteals.netlify.app/.netlify/functions/getSneakers?brand_id=${brand_id}&extended=true`,
+      `/.netlify/functions/getSneakers?brand_id=${brand_id}&extended=true`,
       { headers: headers, maxBodyLength: Infinity }
     );
     console.log(response.data.data);
