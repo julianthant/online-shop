de88e61230215dc0884c76de481306fed24b8154
SoyJuju <julianthant@gmail.com>
2023-10-09 12:01:59 +0630

    I DID ITTT

--- src/functions/GetBrands.jsx
+++ src/functions/GetBrands.jsx
@@ -3,8 +3,8 @@ import axios from 'axios';
 export default async function GetSneakers(setBrand) {
   try {
     const response = await axios.get('/.netlify/functions/brands');
-    console.log(response.data);
-    setBrand(response.data);
+    console.log(response.data.data);
+    setBrand(response.data.data);
   } catch (error) {
     console.error('Error fetching brands:', error);
   }
