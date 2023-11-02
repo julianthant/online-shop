f755c7cf376c3aee66e9e862e71ca28d4fcf1ae5
Soy Juju <julianthant@gmail.com>
2023-11-02 21:13:13 +0630

    order pages

---
+++ src/components/main/OrderInfo.jsx
@@ -0,0 +1,137 @@
+import { useAuth } from '../../hooks/useAuth';
+import { useState, useEffect } from 'react';
+import { useParams, Link } from 'react-router-dom';
+
+export default function OrderInfo() {
+  const [order, setOrder] = useState();
+  const [error, setError] = useState('');
+  const [date, setDate] = useState(null);
+
+  const { currentUser, getOrder } = useAuth();
+  const { orderID } = useParams();
+
+  useEffect(() => {
+    getOrder(orderID, setOrder, setDate, setError);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    currentUser &&
+    order && (
+      <section className="bg-matte-black text-white">
+        <div className="container flex flex-col justify-center gap-6 pt-[6rem] pb-12">
+          <h1 className="text-slate-50 text-5xl font-bold font-[Montserrat] py-5 text-center">
+            Order Details
+            <span className="text-emerald-900">.</span>
+          </h1>
+          <h3 className="text-lg font-bold border-b-[1px] px-5 border-gray-400 border-opacity-70 h-8">
+            PRODUCTS
+          </h3>
+          {order.cartItems.map((item, index) => (
+            <div
+              key={item.id}
+              className={`pt-4 grid gap-2 ${
+                index !== order.cartItems.length - 1
+                  ? 'border-b-[1px] border-gray-400 pb-12 border-opacity-40'
+                  : 'pb-4'
+              }`}
+            >
+              <div className="flex items-center justify-between h-10 px-5">
+                <h4 className="text-base font-bold">NAME</h4>
+                <Link
+                  to={`/sneaker-grid/${item.brand}/${item.id}`}
+                  className="text-base font-bold text-blue-700 underline"
+                >
+                  {item.name}
+                </Link>
+              </div>
+              <div className="flex items-center justify-between h-10 px-5 bg-[#1b1b1b]">
+                <h4 className="text-base font-bold">BRAND</h4>
+                <h4 className="text-base font-bold">{item.brand}</h4>
+              </div>
+              <div className="flex items-center justify-between h-10 px-5">
+                <h4 className="text-base font-bold">ORDERED DATE</h4>
+                <h4 className="text-base font-bold">
+                  {date && date.toDateString()}
+                </h4>
+              </div>
+              <div className="flex items-center justify-between h-10 px-5 bg-[#1b1b1b]">
+                <h4 className="text-base font-bold">ETA</h4>
+                <h4 className="text-base font-bold">2 to 3 weeks</h4>
+              </div>
+              <div className="flex items-center justify-between h-10 px-5">
+                <h4 className="text-base font-bold">PRICE</h4>
+                <h4 className="text-base font-bold">${item.price}.00 USD</h4>
+              </div>
+              <div className="flex items-center justify-between h-10 px-5 bg-[#1b1b1b]">
+                <h4 className="text-base font-bold">QUANTITY</h4>
+                <h4 className="text-base font-bold">
+                  {item.quantity} {item.quantity > 1 ? 'Items' : 'Item'}
+                </h4>
+              </div>
+              <div className="flex items-center justify-between h-10 px-5">
+                <h4 className="text-base font-bold">TOTAL</h4>
+                <h4 className="text-base font-bold">
+                  ${item.price * item.quantity}.00 USD
+                </h4>
+              </div>
+            </div>
+          ))}
+          <h3 className="text-lg px-5 font-bold border-b-[1px] border-gray-400 border-opacity-70 h-8">
+            ADDITIONAL INFO
+          </h3>
+          <div className="py-4 grid gap-2">
+            <div className="flex items-center justify-between h-10 px-5">
+              <h4 className="text-base font-bold">ORDERED DATE</h4>
+              <h4 className="text-base font-bold">
+                {date && date.toDateString()}
+              </h4>
+            </div>
+            <div className="flex items-center justify-between h-10 px-5 bg-[#1b1b1b]">
+              <h4 className="text-base font-bold">ETA</h4>
+              <h4 className="text-base font-bold">2 to 3 weeks</h4>
+            </div>
+            <div className="flex items-center justify-between h-10 px-5">
+              <h4 className="text-base font-bold">STATUS</h4>
+              <h4 className="text-base font-bold">Pending</h4>
+            </div>
+            <div className="flex items-center justify-between h-10 px-5 bg-[#1b1b1b]">
+              <h4 className="text-base font-bold">TOTAL ITEMS</h4>
+              <h4 className="text-base font-bold">
+                {order.totalItems} {order.totalItems > 1 ? 'Items' : 'Item'}
+              </h4>
+            </div>
+          </div>
+          <h3 className="text-lg px-5 font-bold border-b-[1px] border-gray-400 border-opacity-70 h-8">
+            ADDITIONAL COSTS
+          </h3>
+          <div className="py-4 grid gap-2 border-b-[1px] border-gray-400 border-opacity-70">
+            <div className="flex items-center justify-between h-10 px-5">
+              <h4 className="text-base font-bold">SHIPPING</h4>
+              <h4 className="text-base font-bold">
+                ${order.totalItems > 0 ? 5 : 0}.00 USD
+              </h4>
+            </div>
+            <div className="flex items-center justify-between h-10 px-5 bg-[#1b1b1b]">
+              <h4 className="text-base font-bold">DISCOUNT</h4>
+              <h4 className="text-base font-bold">&mdash;</h4>
+            </div>
+            <div className="flex items-center justify-between h-10 px-5 pb-4">
+              <h4 className="text-base font-bold">TAX</h4>
+              <h4 className="text-base font-bold">
+                ${order.totalItems * 1}.00 USD
+              </h4>
+            </div>
+          </div>
+          <div className="flex items-center justify-between h-10 px-5 py-8">
+            <h4 className="text-base font-bold">TOTAL COST</h4>
+            <h4 className="text-base font-bold">
+              ${order.totalPrice + order.addCosts}.00 USD
+            </h4>
+          </div>
+          {error && <p className="text-red-700 text-md mb-3">{error}</p>}
+        </div>
+      </section>
+    )
+  );
+}
--- src/components/main/Orders.jsx
+++ src/components/main/Orders.jsx
@@ -1,7 +1,136 @@
+import { useState, useEffect } from 'react';
+import { useAuth } from '../../hooks/useAuth';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { Timestamp } from 'firebase/firestore';
+
 export default function Orders() {
+  const [orders, setOrders] = useState([]);
+  const { currentUser, getItem } = useAuth();
+
+  useEffect(() => {
+    getItem(setOrders, 'users_orders');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
-    <section className="bg-matte-black">
-      <div>Orders Page</div>
-    </section>
+    <div className="bg-matte-black lg:mt-[10rem] text-white order-container px-5">
+      {currentUser && orders && (
+        <div className="xl:gap-28 gap-16 xl:pl-16 max-xl:grid">
+          <table className="w-full max-md:hidden">
+            <thead>
+              <tr className="border-b-[1px] border-gray-400 lm:py-4 h-10 text-left text-slate-50 text-lg">
+                <th className="max-lm:text-4xl max-lm:text-center font-semibold font-[Poppins] pl-4">
+                  Order
+                </th>
+                <th className="font-semibold font-[Poppins] max-lm:hidden">
+                  Date
+                </th>
+                <th className="font-semibold font-[Poppins] max-lm:hidden">
+                  Quantity
+                </th>
+                <th className="font-semibold font-[Poppins] max-lm:hidden">
+                  Status
+                </th>
+                <th className="font-semibold text-right font-[Poppins] max-lm:hidden pr-4">
+                  Total
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="h-3"></tr>
+              {orders.map((item) => (
+                <OrderItem key={item.id} item={item} />
+              ))}
+            </tbody>
+          </table>
+          <div className="md:hidden">
+            <h1 className="font-bold text-4xl text-slate-50 text-center mb-2">
+              Orders
+            </h1>
+            {orders.map((item) => (
+              <ul
+                key={item.id}
+                className="py-8 grid gap-2 border-b-[1px] border-gray-400"
+              >
+                <li className="flex items-center justify-between h-10 px-5">
+                  <h4 className="text-base font-bold">Order ID</h4>
+                  <h4 className="text-base font-bold text-blue-700 underline">
+                    #<Link to={`/order/${item.id}`}>{item.id}</Link>
+                  </h4>
+                </li>
+
+                <li className="flex items-center justify-between h-10 px-5 bg-[#1B1B1B]">
+                  <h4 className="text-base font-bold">DATE</h4>
+                  <h4 className="text-base font-bold">
+                    {formatDate(item.createdAt.toDate())}
+                  </h4>
+                </li>
+                <li className="flex items-center justify-between h-10 px-5">
+                  <h4 className="text-base font-bold">QUANTITY</h4>
+                  <h4 className="text-base font-bold">
+                    {item.totalItems} Items
+                  </h4>
+                </li>
+                <li className="flex items-center justify-between h-10 px-5 bg-[#1B1B1B]">
+                  <h4 className="text-base font-bold">STATUS</h4>
+                  <h4 className="text-base font-bold">Pending</h4>
+                </li>
+                <li className="flex items-center justify-between h-10 px-5">
+                  <h4 className="text-base font-bold">TOTAL</h4>
+                  <h4 className="text-base font-bold">
+                    ${item.totalPrice}.00 USD
+                  </h4>
+                </li>
+              </ul>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
   );
 }
+
+function formatDate(date) {
+  return new Intl.DateTimeFormat('en-US', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }).format(date);
+}
+
+function OrderItem({ item }) {
+  let date = item.createdAt.toDate();
+
+  return (
+    <tr className="font-[Poppins] h-10 tex5-left text-sm mobile-order">
+      <td className="text-left pl-4 text-blue-700 underline">
+        #<Link to={`/order/${item.id}`}>{item.id}</Link>
+      </td>
+      <td>{date ? formatDate(date) : ''}</td>
+      <td>
+        {item.totalItems} {item.totalItems > 1 ? 'Items' : 'Item'}
+      </td>
+      <td>Pending</td>
+      <td className="text-right w-28 pr-4">${item.totalPrice}.00 USD</td>
+    </tr>
+  );
+}
+
+OrderItem.propTypes = {
+  item: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+    cartItems: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        price: PropTypes.number,
+        image: PropTypes.string,
+        quantity: PropTypes.number,
+      })
+    ).isRequired,
+    createdAt: PropTypes.instanceOf(Timestamp),
+    totalPrice: PropTypes.number,
+    totalItems: PropTypes.number,
+  }).isRequired,
+};
