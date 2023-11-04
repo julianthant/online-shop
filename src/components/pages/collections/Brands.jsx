f6671e9de41a057a78f7b61d8d353a99b488098c
Soy Juju <julianthant@gmail.com>
2023-11-04 18:10:52 +0630

    modified looks

--- src/components/pages/collections/Brands.jsx
+++ src/components/pages/collections/Brands.jsx
@@ -7,14 +7,14 @@ export default function Brands() {
   return (
     <div className="container pt-5">
       <h2 className="font-[Poppins] text-2xl text-slate-50 pb-5">Brands</h2>
-      <div className="grid grid-cols-4 max-lg:grid-cols-3 max-sm:grid-cols-2 gap-5">
+      <div className="flex flex-wrap gap-5">
         {BrandsList.map((brand) => (
           <button
             key={brand.id}
             onClick={() => navigate(`/sneaker-grid/${brand.name}`)}
-            className="rounded-md bg-[#1B1B1B] border border-light-gray hover:bg-[#2B2B2B] transition-colors duration-300 cursor-pointer"
+            className="rounded-md basis-[200px] flex-grow bg-gray-300 border-2 hover:bg-[#b1aeae] transition-colors duration-300 cursor-pointer"
           >
-            <p className="px-5 py-4 text-white text-center font-semibold">
+            <p className="px-5 py-4 text-black text-center font-semibold">
               {brand.name}
             </p>
           </button>
