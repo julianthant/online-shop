c155ec70bacf98ad1f2b78daa3fff50f69307da2
Soy Juju <julianthant@gmail.com>
2023-11-04 23:42:10 +0630

    added brand filter for genders

--- src/components/pages/sneakers/grid/GenderFilter.jsx
+++ src/components/pages/sneakers/grid/GenderFilter.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-import GeneratePrice from './GeneratePrice';
 
 export default function GenderFilter({
   genderFilters,
   setGenderFilters,
   sneakers,
   priceRange,
+  brandFilters,
 }) {
   const [genderCounts, setGenderCounts] = useState({
     men: 0,
@@ -23,34 +23,19 @@ export default function GenderFilter({
       };
 
       sneakers.forEach((sneaker) => {
-        const getSneakerPrice = (sneaker) => {
-          if (sneaker.initialPrice !== null) {
-            return parseInt(sneaker.initialPrice);
-          } else {
-            // Handle the case when initialPrice is null
-            return GeneratePrice(sneaker.id);
-          }
-        };
-
-        const sneakerPrice = getSneakerPrice(sneaker);
-        const priceFilter =
-          sneakerPrice >= priceRange[0] && sneakerPrice <= priceRange[1];
-
-        if (priceFilter) {
-          if (sneaker.sizing === 'man') {
-            counts.men++;
-          } else if (sneaker.sizing === 'woman') {
-            counts.women++;
-          } else {
-            counts.unisex++;
-          }
+        if (sneaker.sizing === 'man') {
+          counts.men++;
+        } else if (sneaker.sizing === 'woman') {
+          counts.women++;
+        } else {
+          counts.unisex++;
         }
       });
       setGenderCounts(counts);
     };
 
     calculateGenderCounts();
-  }, [sneakers, priceRange]);
+  }, [sneakers, priceRange, brandFilters]);
 
   const handleGenderFilterChange = (gender) => {
     setGenderFilters((prevFilters) => ({
@@ -107,4 +92,5 @@ GenderFilter.propTypes = {
     })
   ).isRequired,
   priceRange: PropTypes.arrayOf(PropTypes.number).isRequired,
+  brandFilters: PropTypes.arrayOf(PropTypes.string).isRequired,
 };
