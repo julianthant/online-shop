ebd0aea7fc65a06ce4e3b7cbc5e5511e0d761254
Soy Juju <julianthant@gmail.com>
2023-11-06 23:29:38 +0630

    new contacts page

--- src/components/pages/contacts/ContactsPage.jsx
+++ src/components/pages/contacts/ContactsPage.jsx
@@ -1,7 +1,142 @@
+import SocialIcons from '../../../data/SocialIcons';
+
 export default function ContactsPage() {
   return (
-    <section className="bg-matte-black">
-      <div>Contacts Page</div>
+    <section className="pt-[6rem] bg-matte-black">
+      <div className="container mx-auto px-4">
+        <div className=" bg-[#1B1B1B] text-white rounded-lg p-8 shadow-lg">
+          <h1 className="text-3xl font-semibold mb-6">Contact Us</h1>
+          <p className="text-gray-500">
+            If you have any questions or inquiries, please feel free to contact
+            us using the information below or the contact form.
+          </p>
+          <div className="mt-6">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+              <div>
+                <h2 className="text-xl font-semibold">Contact Details</h2>
+                <ul className="mt-4">
+                  <li className="flex items-start text-gray-500 mb-4">
+                    <span className="w-6 h-6 text-gray-400 mr-2">
+                      <svg
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M9 19l-7-7 7-7 7 7-7 7zm0 0v-14"
+                        ></path>
+                      </svg>
+                    </span>
+                    <div>
+                      <strong>Email:</strong> info@yourshoestore.com
+                    </div>
+                  </li>
+                  <li className="flex items-start text-gray-500 mb-4">
+                    <span className="w-6 h-6 text-gray-400 mr-2">
+                      <svg
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M9 19l-7-7 7-7 7 7-7 7zm0 0v-14"
+                        ></path>
+                      </svg>
+                    </span>
+                    <div>
+                      <strong>Phone:</strong> +1 (123) 456-7890
+                    </div>
+                  </li>
+                  <li className="flex items-start text-gray-500">
+                    <span className="w-6 h-6 text-gray-400 mr-2">
+                      <svg
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M9 19l-7-7 7-7 7 7-7 7zm0 0v-14"
+                        ></path>
+                      </svg>
+                    </span>
+                    <div>
+                      <strong>Address:</strong> 123 Shoe Street, City, Country
+                    </div>
+                  </li>
+                </ul>
+                <div className="mt-6">
+                  <SocialIcons />
+                </div>
+              </div>
+              <div>
+                <h2 className="text-xl font-semibold">Contact Form</h2>
+                <form className="mt-4">
+                  <div className="mb-4">
+                    <label
+                      htmlFor="name"
+                      className="block text-gray-600 font-medium"
+                    >
+                      Name
+                    </label>
+                    <input
+                      type="text"
+                      id="name"
+                      name="name"
+                      className="w-full px-4 py-2 border rounded-lg outline-none bg-[#28282B]"
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="email"
+                      className="block text-gray-600 font-medium"
+                    >
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      id="email"
+                      name="email"
+                      className="w-full px-4 py-2 border rounded-lg outline-none bg-[#28282B]"
+                      required
+                    />
+                  </div>
+                  <div className="mb-4">
+                    <label
+                      htmlFor="message"
+                      className="block text-gray-600 font-medium"
+                    >
+                      Message
+                    </label>
+                    <textarea
+                      id="message"
+                      name="message"
+                      rows="5"
+                      className="w-full px-4 py-2 border rounded-lg outline-none bg-[#28282B]"
+                      required
+                    ></textarea>
+                  </div>
+                  <button
+                    type="submit"
+                    className="w-full bg-emerald-500 text-white py-2 rounded-lg hover:bg-emerald-600 transition duration-300"
+                  >
+                    Send Message
+                  </button>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   );
 }
