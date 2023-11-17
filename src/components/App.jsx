18205c85b31e1dd4c8470bc1f5824a405bac2957
Soy Juju <julianthant@gmail.com>
2023-11-17 11:21:24 +0630

    changed app routings and added new context

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,21 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { AuthProvider } from '../contexts/AuthContexts';
+import { QuantityProvider } from '../contexts/QuantityContext';
+
+import Navbar from './main/Navbar';
 import Homepage from './pages/home/Homepage';
 import PrivateRoute from '../hooks/PrivateRoute';
-import { FirebaseProvider } from '../contexts/FirebaseContexts';
-import Navbar from './main/Navbar';
-import SneakerDetails from './pages/sneakers/details/SneakerDetails';
-import OrderInfo from './main/OrderInfo';
 
 const Collections = lazy(() => import('./pages/collections/CollectionsPage'));
+const SneakerDetails = lazy(() =>
+  import('./pages/sneakers/details/SneakerDetails')
+);
 
 const CartPage = lazy(() => import('./pages/cart/CartPage'));
 const ContactsPage = lazy(() => import('./pages/contacts/ContactsPage'));
 const CheckoutPage = lazy(() => import('./pages/cart/Checkout'));
+const OrderInfo = lazy(() => import('./main/OrderInfo'));
 
 const Dashboard = lazy(() => import('./main/Dashboard'));
 const Login = lazy(() => import('./authentication/Login'));
@@ -25,43 +29,50 @@ const EmailVerification = lazy(() =>
 function App() {
   return (
     <Router>
-      <FirebaseProvider>
-        <Navbar />
-        <Suspense
-          fallback={
-            <div className="bg-matte-black min-h-screen w-screen">
-              <Navbar fallbackClass="bg-matte-black" />
-            </div>
-          }
-        >
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/dashboard" element={<PrivateRoute />}>
-              <Route index element={<Dashboard />} />
-            </Route>
-            <Route path="/forgot-password" element={<ForgotPassword />} />
-            <Route path="/verify-email" element={<EmailVerification />} />
-            <Route path="/action-center" element={<PrivateRoute />}>
-              <Route index element={<AccountCenter />} />
-            </Route>
-            <Route path="/collections" element={<Collections />} />
-            <Route path="/cart" element={<CartPage />} />
-            <Route path="/checkout" element={<PrivateRoute />}>
-              <Route index element={<CheckoutPage />} />
-            </Route>
-            <Route path="/order/:orderID" element={<PrivateRoute />}>
-              <Route index element={<OrderInfo />} />
-            </Route>
-            <Route path="/contacts" element={<ContactsPage />} />
-            <Route
-              path="/sneaker-grid/:brandName/:sneakerID"
-              element={<SneakerDetails />}
-            />
-          </Routes>
-        </Suspense>
-      </FirebaseProvider>
+      <AuthProvider>
+        <QuantityProvider>
+          <Navbar />
+          <Suspense
+            fallback={
+              <div className="bg-matte-black min-h-screen w-screen">
+                <Navbar fallbackClass="bg-matte-black" />
+              </div>
+            }
+          >
+            <Routes>
+              <Route path="/" element={<Homepage />} />
+              <Route path="/signup" element={<Signup />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/forgot-password" element={<ForgotPassword />} />
+              <Route path="/verify-email" element={<EmailVerification />} />
+              <Route path="/contacts" element={<ContactsPage />} />
+              <Route path="/collections" element={<Collections />} />
+              <Route path="/cart" element={<CartPage />} />
+
+              <Route path="/dashboard" element={<PrivateRoute />}>
+                <Route index element={<Dashboard />} />
+              </Route>
+
+              <Route path="/action-center" element={<PrivateRoute />}>
+                <Route index element={<AccountCenter />} />
+              </Route>
+
+              <Route path="/checkout" element={<PrivateRoute />}>
+                <Route index element={<CheckoutPage />} />
+              </Route>
+
+              <Route path="/order/:orderID" element={<PrivateRoute />}>
+                <Route index element={<OrderInfo />} />
+              </Route>
+
+              <Route
+                path="/sneaker-grid/:brandName/:sneakerID"
+                element={<SneakerDetails />}
+              />
+            </Routes>
+          </Suspense>
+        </QuantityProvider>
+      </AuthProvider>
     </Router>
   );
 }
